368a10880e1c9fa0f2bf6282f4afa9a3afbd2c25
Ardash <ardache@hotmail.com>
2020-03-17 20:41:50 -0600

    conf: app.js configuration

--- pcp-server/app.js
+++ pcp-server/app.js
@@ -8,6 +8,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const cors         = require('cors');
 
 
 mongoose
@@ -44,7 +45,12 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:3000'] // <== this will be the URL of our React app (it will be running on port 3000)
+  })
+);
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
