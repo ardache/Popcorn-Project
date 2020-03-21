a66a9285dbe124978ec194c4baa3102ea45a0a77
Ardash <ardache@hotmail.com>
2020-03-21 11:58:30 -0600

    feauture: Adding route (All)

--- pcp-server/routes/question-routes.js
+++ pcp-server/routes/question-routes.js
@@ -4,6 +4,15 @@ const Question = require('../models/Question');
 
 
 /* GET Question by id. */
+router.get('/questions', (req, res, next) => {
+    Question.find()
+    .then(questions=>{
+        res.status(200).json({questions})
+    })
+    .catch(e=>console.log(e))
+});
+
+/* GET Question by id. */
 router.get('/questions/:id', (req, res, next) => {
     Question.findById(req.params.id)
     .then(question=>{
